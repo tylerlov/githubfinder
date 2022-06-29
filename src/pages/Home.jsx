c0f2e6e6d2015aa8b54f231503ee2f809b2df3ce
stylotylo <tylerlovell@gmail.com>
2022-06-29 13:25:43 -0400

    Loading files update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
   return (
-    <h1 className='text-6xl'>Welcome</h1>
-    
+    <>
+        <h1 className='text-6xl'>Welcome</h1>
+
+        <UserResults />
+    </>
   )
 }
 
